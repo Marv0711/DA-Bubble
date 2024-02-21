23cb2db77daf3d4b057736c97c306a455ed6ec01
JanFromTheBlock <133579648+JanFromTheBlock@users.noreply.github.com>
2024-02-21 17:09:11 +0100

    create & design workspace-menu

--- src/app/main-board/main-board.component.ts
+++ src/app/main-board/main-board.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { BoardHeaderComponent } from "./board-header/board-header.component";
+import { WorkspaceMenuComponent } from './workspace-menu/workspace-menu.component';
 
 @Component({
     selector: 'app-main-board',
     standalone: true,
     templateUrl: './main-board.component.html',
     styleUrl: './main-board.component.scss',
-    imports: [BoardHeaderComponent]
+    imports: [BoardHeaderComponent, WorkspaceMenuComponent]
 })
 export class MainBoardComponent {
 
