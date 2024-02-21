00818c4c92e6a6b7bb1397218c6e2dbcd1105fca
Marv0711 <marvin.buchan@t-online.de>
2024-02-21 14:51:28 +0100

    add angular-material

--- src/app/main-board/board-header/board-header.component.ts
+++ src/app/main-board/board-header/board-header.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { LogoComponent } from "../../logo/logo.component";
 
 @Component({
-  selector: 'app-board-header',
-  standalone: true,
-  imports: [],
-  templateUrl: './board-header.component.html',
-  styleUrl: './board-header.component.scss'
+    selector: 'app-board-header',
+    standalone: true,
+    templateUrl: './board-header.component.html',
+    styleUrl: './board-header.component.scss',
+    imports: [LogoComponent]
 })
 export class BoardHeaderComponent {
 
