3da77895dcdb9fa9bb57198c2ae23b3da61169aa
Marv0711 <marvin.buchan@t-online.de>
2024-02-21 12:54:04 +0100

    main-board in app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MainBoardComponent } from "./main-board/main-board.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, MainBoardComponent]
 })
 export class AppComponent {
   title = 'da-bubble-project';
--- src/app/main-board/main-board.component.ts
+++ src/app/main-board/main-board.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { BoardHeaderComponent } from "./board-header/board-header.component";
 
 @Component({
-  selector: 'app-main-board',
-  standalone: true,
-  imports: [],
-  templateUrl: './main-board.component.html',
-  styleUrl: './main-board.component.scss'
+    selector: 'app-main-board',
+    standalone: true,
+    templateUrl: './main-board.component.html',
+    styleUrl: './main-board.component.scss',
+    imports: [BoardHeaderComponent]
 })
 export class MainBoardComponent {
 
