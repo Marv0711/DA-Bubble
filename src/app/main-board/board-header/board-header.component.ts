a1697a2e1403c1eca1cf91a3f38dce49d168bac9
Marv0711 <marvin.buchan@t-online.de>
2024-02-21 18:25:40 +0100

    load fonts

--- src/app/main-board/board-header/board-header.component.ts
+++ src/app/main-board/board-header/board-header.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { LogoComponent } from "../../logo/logo.component";
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
     selector: 'app-board-header',
     standalone: true,
     templateUrl: './board-header.component.html',
     styleUrl: './board-header.component.scss',
-    imports: [LogoComponent]
+    imports: [LogoComponent, MatIconModule]
 })
 export class BoardHeaderComponent {
 
