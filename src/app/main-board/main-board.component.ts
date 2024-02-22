b8bb4f0bd597a697757b491c40ff87f0f2fc29e5
Marv0711 <marvin.buchan@t-online.de>
2024-02-22 12:29:46 +0100

    create Messagefield

--- src/app/main-board/main-board.component.ts
+++ src/app/main-board/main-board.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { BoardHeaderComponent } from "./board-header/board-header.component";
 import { WorkspaceMenuComponent } from './workspace-menu/workspace-menu.component';
+import { MessageFieldComponent } from "../message-field/message-field.component";
 
 @Component({
     selector: 'app-main-board',
     standalone: true,
     templateUrl: './main-board.component.html',
     styleUrl: './main-board.component.scss',
-    imports: [BoardHeaderComponent, WorkspaceMenuComponent]
+    imports: [BoardHeaderComponent, WorkspaceMenuComponent, MessageFieldComponent]
 })
 export class MainBoardComponent {
 
---
+++ src/app/message-field/message-field.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-message-field',
+  standalone: true,
+  imports: [],
+  templateUrl: './message-field.component.html',
+  styleUrl: './message-field.component.scss'
+})
+export class MessageFieldComponent {
+
+}
