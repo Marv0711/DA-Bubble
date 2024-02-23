389ff3260bc9a6a1fe207970dc87a573fa86c651
Marv0711 <marvin.buchan@t-online.de>
2024-02-23 17:09:30 +0100

    add smiley

--- src/app/main-board/message-field/message-field.component.ts
+++ src/app/main-board/message-field/message-field.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { PickerModule } from "@ctrl/ngx-emoji-mart";
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';  
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-message-field',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule,PickerModule,FormsModule,CommonModule],
   templateUrl: './message-field.component.html',
   styleUrl: './message-field.component.scss'
 })
 export class MessageFieldComponent {
+
+  public textAreaInput:string = '';
+  isEmojiPickerVisible:boolean = false;
+
+  addEmoji(event:any){
+    this.textAreaInput = `${this.textAreaInput}${event.emoji.native}`;
+    this.isEmojiPickerVisible = false;
+  }
 
 }
