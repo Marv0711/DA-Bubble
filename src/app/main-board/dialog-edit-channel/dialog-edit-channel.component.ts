22dab2be515957a6d699729d816bea872de53641
KevinFunck <129848357+KevinFunck@users.noreply.github.com>
2024-02-24 14:26:40 +0100

    transform channel name and save

--- src/app/main-board/dialog-edit-channel/dialog-edit-channel.component.ts
+++ src/app/main-board/dialog-edit-channel/dialog-edit-channel.component.ts
@@ -21,6 +21,23 @@ export class DialogEditChannelComponent {
   }
 
   editChannelName() {
-    
+   this.transformChannelNameAndSave();
+    document.getElementById('edit')?.classList.add('d-none');
+    document.getElementById('save')?.classList.remove('d-none');
+    document.getElementById('nameOfChannel')?.classList.add('d-none');
+    document.getElementById('inputChannelName')?.classList.remove('d-none');
+    document.getElementById('channelBorder')?.classList.remove('border');
+  }
+
+  transformChannelNameAndSave() {
+    const channelAndSave = document.getElementById('save');
+    const channelName = document.getElementById('channelName');
+    if (channelAndSave && channelName) {
+      channelName.style.transition = "transform 0.1s ease";
+      channelName.style.transform = "translateY(-20px)";
+      channelAndSave.style.transition = "transform 5.0s ease";
+      channelAndSave.style.transform = "translateY(-20px)";
+    }
+
   }
 }
