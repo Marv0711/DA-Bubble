f94be7c3389ce7e7924717a68714d1fc111bc501
JanFromTheBlock <133579648+JanFromTheBlock@users.noreply.github.com>
2024-02-25 16:16:45 +0100

    change img of togglebar menu

--- src/app/main-board/workspace-menu-togglebar/workspace-menu-togglebar.component.ts
+++ src/app/main-board/workspace-menu-togglebar/workspace-menu-togglebar.component.ts
@@ -13,13 +13,19 @@ export class WorkspaceMenuTogglebarComponent {
 
   toggleWorkspaceMenu(){
     let workspaceText: any = document.getElementById('togglebar-text');
+    let workspaceImg: any = document.getElementById('togglebar-img');
     if (this.workspaceOpen) {
       document.getElementById('app-workspace-menu')?.classList.add('d-none');
       workspaceText.innerHTML = 'Workspace-Menü öffnen'
+      workspaceImg.src = 'assets/img/show_navigation.png'
+      workspaceImg.classList.add('img-close');
       this.workspaceOpen = false;
     }else{
       document.getElementById('app-workspace-menu')?.classList.remove('d-none');
       workspaceText.innerHTML = 'Workspace-Menü schließen'
+      workspaceImg.src = 'assets/img/hide-navigation.png'
+      workspaceImg.classList.remove('img-close');
+
       this.workspaceOpen = true;
     }
   }
