9dbff5a7223165f88fcf2abcd7dba7ab5cf014e9
JanFromTheBlock <133579648+JanFromTheBlock@users.noreply.github.com>
2024-02-26 17:11:17 +0100

    update merge

--- src/app/main-board/board-header/board-header.component.ts
+++ src/app/main-board/board-header/board-header.component.ts
@@ -1,10 +1,10 @@
 import { Component, inject } from '@angular/core';
-import { LogoComponent } from "../../logo/logo.component";
 import { MatIconModule } from '@angular/material/icon';
 import { DialogBoarderHeaderComponent } from '../dialog-boarder-header/dialog-boarder-header.component';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { FirestoreServiceService } from '../../../services/firestore-service.service';
 import { Firestore, collection, getDocs } from '@angular/fire/firestore';
+import { LogoComponent } from "../../logo/logo.component";
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -15,7 +15,13 @@ import { CommonModule } from '@angular/common';
     imports: [LogoComponent, MatIconModule, MatDialogModule, CommonModule]
 })
 export class BoardHeaderComponent {
-    constructor(public dialog: MatDialog) { }
+    constructor(public dialog: MatDialog, public firestoreService: FirestoreServiceService) { }
+    firestore: Firestore = inject(Firestore);
+    chatOpenAndWithUnder1200px: boolean = false;
+
+    ngOnInit(){
+        this.checkRightUser('pw', 'mail');
+    }
 
     openDialog() {
         this.dialog.open(DialogBoarderHeaderComponent,{
