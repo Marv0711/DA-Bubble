cc240cc8013069727dd0a6dedaf9824547f23f72
JanFromTheBlock <133579648+JanFromTheBlock@users.noreply.github.com>
2024-02-27 19:05:59 +0100

    bug fixes responsive functions

--- src/app/main-board/board-header/board-header.component.ts
+++ src/app/main-board/board-header/board-header.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, HostListener } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { DialogBoarderHeaderComponent } from '../dialog-boarder-header/dialog-boarder-header.component';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
@@ -20,32 +20,64 @@ export class BoardHeaderComponent {
     firestore: Firestore = inject(Firestore);
     ResponsiveService = inject(OpenChatWindowResponsiveService);
 
-    ngOnInit(){
+    ngOnInit() {
         this.checkRightUser('pw', 'mail');
     }
 
     openDialog() {
-        this.dialog.open(DialogBoarderHeaderComponent,{
+        this.dialog.open(DialogBoarderHeaderComponent, {
             position: {
                 top: '150px',
                 right: '20px',
             },
-            panelClass: 'custom-container' 
+            panelClass: 'custom-container'
         });
     }
 
-    async checkRightUser(pw:string, mail:string){
+    async checkRightUser(pw: string, mail: string) {
         let querySnapshot = await getDocs(collection(this.firestore, 'users'));
         let documentIds;
-        
+
         querySnapshot.forEach((doc) => {
             const data = doc.data();
-            if (data && data['password'] === pw && data['email'] === mail ) {
+            if (data && data['password'] === pw && data['email'] === mail) {
                 let docRef = this.firestoreService.getUser(doc.id);
                 this.firestoreService.getUserJSON(docRef);
             }
-          });
-        
-          return documentIds;
+        });
+
+        return documentIds;
     }
+
+    closeChannel() {
+        let workspaceMenu = document.getElementById('app-workspace-menu');
+        let channelChatWindow = document.getElementById('app-channel-chat-window');
+        if (workspaceMenu && channelChatWindow) {
+            workspaceMenu.style.display = 'flex';
+            channelChatWindow.style.display = 'none';
+        }
+        
+    }
+
+    @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    if (window.innerWidth > 1200 && this.ResponsiveService.chatOpenAndWithUnder1200px) {
+        this.ResponsiveService.chatOpenAndWithUnder1200px = false;
+        }
+        let workspaceMenu = document.getElementById('app-workspace-menu');
+        let channelChatWindow = document.getElementById('app-channel-chat-window');
+        if(workspaceMenu && channelChatWindow && window.innerWidth > 1200){
+            workspaceMenu.style.display = 'flex';
+            channelChatWindow.style.display = 'flex';
+    }
+    if (window.innerWidth < 1200 && !this.ResponsiveService.chatOpenAndWithUnder1200px) {
+        let channelChatWindow = document.getElementById('app-channel-chat-window');
+        let workspaceMenu = document.getElementById('app-workspace-menu');
+        if(channelChatWindow && workspaceMenu){
+            channelChatWindow.style.display = 'none';
+            workspaceMenu.classList.remove('d-none')
+        }
+    }
+  }
+  
 }
