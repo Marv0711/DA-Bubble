76409b63c5232b7f2f4ad9089fbd3f5f2ad07eff
KevinFunck <129848357+KevinFunck@users.noreply.github.com>
2024-03-03 13:31:18 +0100

    work on the threat

--- src/app/main-board/thread-window/thread-window.component.ts
+++ src/app/main-board/thread-window/thread-window.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 
+
 @Component({
-  selector: 'app-thread-window',
-  standalone: true,
-  imports: [],
-  templateUrl: './thread-window.component.html',
-  styleUrl: './thread-window.component.scss'
+    selector: 'app-thread-window',
+    standalone: true,
+    templateUrl: './thread-window.component.html',
+    styleUrl: './thread-window.component.scss',
+    imports: []
 })
 export class ThreadWindowComponent {
+
+  closeThread() {
+    document.getElementById('threat')?.classList.add('d-none');
+  }
 
 }
