85014b61fd8a38277a0b4f3ef6b0b32d3c79c992
Daniel San <106621051+DanielBlem93@users.noreply.github.com>
2024-03-09 18:11:08 +0100

    added reset passwort validation

--- src/app/login/reset-passwort/reset-passwort.component.ts
+++ src/app/login/reset-passwort/reset-passwort.component.ts
@@ -5,14 +5,23 @@ import { FooterComponent } from '../footer/footer.component';
 import { MatIcon } from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { Form, FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-reset-passwort',
   standalone: true,
-  imports: [MatCard,MatCardHeader, MatCardContent,HeaderComponent, FooterComponent,MatIcon,CommonModule,RouterLink ],
+  imports: [MatCard, MatCardHeader, MatCardContent, HeaderComponent, FooterComponent, MatIcon, CommonModule, RouterLink, FormsModule],
   templateUrl: './reset-passwort.component.html',
   styleUrl: './reset-passwort.component.scss'
 })
 export class ResetPasswortComponent {
   isDisabled = false
+
+  password!: string
+  passwordRepeat!: string
+
+
+  onSubmit(myform: Form) {
+    let form = myform
+  }
 }
