b830b9763d25ce640bbb2364be91e6ac56410838
KevinFunck <129848357+KevinFunck@users.noreply.github.com>
2024-03-24 09:38:43 +0100

    d

--- src/app/main-board/new-message/new-message.component.ts
+++ src/app/main-board/new-message/new-message.component.ts
@@ -4,13 +4,17 @@ import { MessageFieldComponent } from '../message-field/message-field.component'
 @Component({
   selector: 'app-new-message',
   standalone: true,
-  imports: [MessageFieldComponent, ],
+  imports: [MessageFieldComponent,],
   templateUrl: './new-message.component.html',
   styleUrl: './new-message.component.scss'
 })
 export class NewMessageComponent {
 
+  /**
+ * Prevents event propagation.
+ * @param event The event object.
+ */
   dontclose(event: Event) {
     event.stopPropagation();
-};
+  };
 }
