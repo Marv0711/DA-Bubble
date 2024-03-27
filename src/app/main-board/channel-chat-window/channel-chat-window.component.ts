83cbac947f61a085b5aa921a8cdabf8ad563536a
KevinFunck <129848357+KevinFunck@users.noreply.github.com>
2024-03-27 20:23:42 +0100

    g

--- src/app/main-board/channel-chat-window/channel-chat-window.component.ts
+++ src/app/main-board/channel-chat-window/channel-chat-window.component.ts
@@ -163,6 +163,6 @@ export class ChannelChatWindowComponent {
     document.querySelector('.time')?.classList.add('flipped');
     document.querySelector('.answers')?.classList.add('flipped');
     document.querySelector('.answer')?.classList.add('flipped');
-    document.querySelector('.emojis')?.classList.add('flipped');
+    document.querySelector('.reaction-area')?.classList.add('flipped');
   }
 }
