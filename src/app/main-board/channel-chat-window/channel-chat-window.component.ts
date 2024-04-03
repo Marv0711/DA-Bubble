8b3e469434f636cb3c25561b2b7d1faea82d7115
KevinFunck <129848357+KevinFunck@users.noreply.github.com>
2024-04-03 09:51:45 +0200

    change the chat img

--- src/app/main-board/channel-chat-window/channel-chat-window.component.ts
+++ src/app/main-board/channel-chat-window/channel-chat-window.component.ts
@@ -179,7 +179,6 @@ export class ChannelChatWindowComponent {
       document.querySelector('.message-div')?.classList.add('flipped');
       document.querySelector('h3')?.classList.add('flipped');
       document.querySelector('.text-question-channel')?.classList.add('flipped');
-      document.querySelector('.message-img')?.classList.add('flipped');
       document.querySelector('.time')?.classList.add('flipped');
       document.querySelector('.answers')?.classList.add('flipped');
       document.querySelector('.answer')?.classList.add('flipped');
