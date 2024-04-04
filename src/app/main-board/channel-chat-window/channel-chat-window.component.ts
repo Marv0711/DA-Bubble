99d4a12acb2d2dc606e7aedfdbeee90638efa3ba
Marv0711 <marvin.buchan@t-online.de>
2024-04-04 23:15:22 +0200

    edit message design btn and bug fix

--- src/app/main-board/channel-chat-window/channel-chat-window.component.ts
+++ src/app/main-board/channel-chat-window/channel-chat-window.component.ts
@@ -81,11 +81,12 @@ export class ChannelChatWindowComponent {
     this.newText[i] = chat.textAreaInput;
   }
 
-  EditChat(chatID:string, i:number) {
+  EditChat(chat:any, chatID:string, i:number) {
     let newText = this.newText[i]
     this.chatService.editChat(chatID, 'chat', newText);
     this.threadService.threadChatText = newText;
     this.newText[i] = '';
+    chat.editOpen = false;
   }
 
   noEditChat(i:number, chat:any){
